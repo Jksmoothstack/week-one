c17ec1fd693239e13b200281c64b1968dfb24784
JakesProjects1 <jacob@kagon.net>
2021-04-05 18:25:25 -0700

    done with assignmentOne

--- PatternPrint.java
+++ PatternPrint.java
@@ -1,6 +1,28 @@
 public class PatternPrint {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("1)");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println(".........");
+        System.out.println("2)");
+        System.out.println("*****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+        System.out.println(".........");
+        System.out.println("3)");
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" ******");
+        System.out.println("********");
+        System.out.println(".........");
+        System.out.println("4)");
+        System.out.println("*******");
+        System.out.println(" *****");
+        System.out.println("  ***");
+        System.out.println("   *");
     }
 }
